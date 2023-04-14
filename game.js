13a46e2dfbc615abaa91a95829c8498afd53a695
AbuUsaid <usaidabu321@gmail.com>
2023-04-15 01:35:29 +0530

    Completed game.js file
    Added the necessary logic to create a rock paper scissors game that runs
    for 5 rounds and displays the score at the end. Also added comments to
    clarify the code and improve readability.

--- game.js
+++ game.js
@@ -6,22 +6,15 @@ let getComputerChoice = () => {
   return options[randomNum];
 };
 
-let computerSelection = getComputerChoice();
+//--------------------
+/*
+ * paper beats rock🪨📃✂️
+ * rock beats scissors
+ * scissors beats paper
+ */
 
-let playerSelection = prompt(
-  "Let's play rock🪨-paper📃-scissors✂️! \nPlease choose 'rock', 'paper', or 'scissors'."
-).toLowerCase();
-
-function playRound(user, comp) {
-  console.log(
-    `The player choice is ${user}, the comp choice is ${computerSelection}`
-  );
-
-  /*
-   * paper beats rock🪨📃✂️
-   * rock beats scissors
-   * scissors beats paper
-   */
+let playRound = (user, comp) => {
+  console.log(`The player choice is ${user}, the comp choice is ${comp}`);
 
   if (comp === 'rock') {
     if (user === 'rock') {
@@ -29,7 +22,7 @@ function playRound(user, comp) {
     } else if (user === 'paper') {
       return 'You Won ! Paper📃 beats Rock🪨';
     } else if (user === 'scissors') {
-      return 'You Lose ! Rock🪨 beats Scissors✂️';
+      return 'You Lose ! Rock🪨 beats Scissors ✂️';
     }
   } else if (comp === 'paper') {
     if (user === 'rock') {
@@ -37,19 +30,67 @@ function playRound(user, comp) {
     } else if (user === 'paper') {
       return "It's a tie for paper!📃";
     } else if (user === 'scissors') {
-      return 'You Won ! Scissors✂️ beats Paper📃';
+      return 'You Won ! Scissors ✂️ beats Paper📃';
     }
   } else if (comp === 'scissors') {
     if (user === 'rock') {
-      return 'You Won ! Rock🪨 beats Scissors✂️';
+      return 'You Won ! Rock🪨 beats Scissors ✂️';
     } else if (user === 'paper') {
-      return 'You Lose ! Scissors✂️ beats Paper📃';
+      return 'You Lose ! Scissors ✂️ beats Paper📃';
     } else if (user === 'scissors') {
       return "It's a tie for scissors!✂️";
     }
   }
-} //playRound()
+}; //playRound()
 
-console.log(playRound(playerSelection, computerSelection));
+//score object
+let score = {
+  user: 0,
+  computer: 0,
+  tie: 0,
+};
 
-//The above function is working fine
+//game of 5 rounds
+for (let i = 0; i < 5; i++) {
+  let playerSelection = prompt(
+    "Let's play rock🪨-paper📃-scissors ✂️! \nPlease choose 'rock', 'paper', or 'scissors'."
+  ).toLowerCase();
+
+  let computerSelection = getComputerChoice();
+
+  let result = playRound(playerSelection, computerSelection);
+  console.log(result);
+
+  if (
+    result === 'You Won ! Paper📃 beats Rock🪨' ||
+    result === 'You Won ! Scissors ✂️ beats Paper📃' ||
+    result === 'You Won ! Rock🪨 beats Scissors ✂️'
+  ) {
+    score.user++;
+  } else if (
+    result === 'You Lose ! Rock🪨 beats Scissors ✂️' ||
+    result === 'You Lose ! Paper📃 beats Rock🪨' ||
+    result === 'You Lose ! Scissors ✂️ beats Paper📃'
+  ) {
+    score.computer++;
+  } else if (
+    result === "It's a tie for rock!🪨" ||
+    result === "It's a tie for paper!📃" ||
+    result === "It's a tie for scissors!✂️"
+  ) {
+    score.tie++;
+  }
+}
+
+console.log(
+  `🏁Final score🏁: User ${score.user} - Computer ${score.computer} - Ties ${score.tie}`
+);
+
+//declaring winnner based on the score
+if (score.user > score.computer) {
+  console.log('You win the game! 🏆');
+} else if (score.user < score.computer) {
+  console.log('You lose the game! 💤');
+} else {
+  console.log("It's a tie! 🤝");
+}
