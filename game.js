a213e05269c583a38c5a1ab9ae03aa358aca6553
AbuUsaid <usaidabu321@gmail.com>
2023-04-14 18:28:02 +0530

    Improve conditional logic and add tie condition

--- game.js
+++ game.js
@@ -6,14 +6,6 @@ let getComputerChoice = () => {
   return options[randomNum];
 };
 
-// function getComputerChoice() {
-//   let randomNum = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
-//   let options = ['rock', 'paper', 'scissors'];
-//   return options[randomNum];
-// }
-
-// console.log(getComputerChoice());
-
 let playerSelection = prompt(
   "Let's play rock-paper-scissors! Please choose 'rock', 'paper', or 'scissors'."
 ).toLowerCase();
@@ -21,8 +13,35 @@ let playerSelection = prompt(
 function playRound(user, comp) {
   //   console.log(`The player choice is ${user}, the comp choice is ${comp()}`);
 
-  if (user === 'rock' && comp() === 'paper') {
-    return 'You Lose! Paper beats Rock';
+  /*
+   * paper beats scissors
+   * scissors beats paper
+   * rock beats scissors
+   */
+  if (comp() === 'rock') {
+    if (user === 'rock') {
+      return "It's a tie for rock!";
+    } else if (user === 'paper') {
+      return 'You Won ! Paper beats Rock';
+    } else if (user === 'scissors') {
+      return 'You Lose ! Rock beats Scissors';
+    }
+  } else if (comp() === 'paper') {
+    if (user === 'rock') {
+      return 'You Lose ! Paper beats Rock';
+    } else if (user === 'paper') {
+      return "It's a tie for paper!";
+    } else if (user === 'scissors') {
+      return 'You Won ! Scissors beats Paper';
+    }
+  } else if (comp() === 'scissors') {
+    if (user === 'rock') {
+      return 'You Won ! Rock beats Scissors';
+    } else if (user === 'paper') {
+      return 'You Lose ! Scissors beats Paper';
+    } else if (user === 'scissors') {
+      return "It's a tie for scissors!";
+    }
   }
 }
 
